76adeff5d1c377b8dd9bc6c60236c92577c83d87
Jorge Martinez <tnlcomputer@gmail.com>
2023-09-21 20:39:52 -0300

    agregados y truco en testing

--- testexample/tests/Unit/UserTest.php
+++ testexample/tests/Unit/UserTest.php
@@ -9,8 +9,8 @@ class UserTest extends TestCase
     /**
      * A basic unit test example.
      */
-    // public function test_example(): void
-    // {
-    //     $this->assertTrue(true);
-    // }
+    public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
 }
